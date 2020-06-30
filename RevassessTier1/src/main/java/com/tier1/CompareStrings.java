4be24922e9e390977e26a0659eae5bd05be24365
CuriousGeek <krit.s255@gmail.com>
2020-06-30 11:30:19 -0400

    update

--- RevassessTier1/src/main/java/com/tier1/CompareStrings.java
+++ RevassessTier1/src/main/java/com/tier1/CompareStrings.java
@@ -3,6 +3,6 @@ package com.tier1;
 public class CompareStrings {
 
     public static boolean compareStrings(String s1, String s2){
-        return s1.contentEquals(s2);
+        return s1.contentEquals(s2); //using contentEquals method on string.
     }
 }
