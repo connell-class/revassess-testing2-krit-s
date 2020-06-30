67707205ff79462d7b11ef6cde7aeb7b91efa9e9
CuriousGeek <krit.s255@gmail.com>
2020-06-29 23:50:49 -0400

    written methods for tier1

--- RevassessTier1/src/main/java/com/tier1/NumberSumLength.java
+++ RevassessTier1/src/main/java/com/tier1/NumberSumLength.java
@@ -3,6 +3,13 @@ package com.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+		int len = String.valueOf(num).length();
+		long sum = 0;
+		long temp = num;
+		for(int i =1; i <= len; i++) {
+			sum += Math.pow(temp%10, len);
+			temp = temp/10;
+		}
+		return (num == sum);
 	}
 }
--- RevassessTier1/src/main/java/com/tier1/SumOverArray.java
+++ RevassessTier1/src/main/java/com/tier1/SumOverArray.java
@@ -3,6 +3,13 @@ package com.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+		int sum = 0;
+		try {
+			for(int i = 0; i < arr.length; i++) {
+				sum += arr[i];
+			}
+		} catch (NullPointerException e) {}
+		
+		return sum;
 	}
 }
